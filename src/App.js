6bf789b9f9b22d3844e1024782affb88f3a99f4e
bimalesh12345 <bimalesh54321@gmail.com>
2019-06-24 16:16:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Container, Row, Col, Button  } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Container>
+        <Row>
+           <Col sm="12">
+           <div className="book-title">Book List</div>
+           <div className="upper-table">
+           <Button color="success">Add Book</Button>
+           </div>
+        <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Book Title</th>
+            <th>Rating</th>
+            <th>Writer</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>My Book</td>
+            <td>4.5</td>
+            <td>Mark</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Discover</td>
+            <td>5.0</td>
+            <td>Jacob</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>the Bird</td>
+            <td>3.8</td>
+            <td>Larry</td>
+          </tr>
+        </tbody>
+      </Table>
+           </Col>
+        </Row>
+        </Container>
+        
     </div>
   );
 }
