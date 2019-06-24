9319cb5c60a1143fbd1b0e0dd61c6db207f68730
bimalesh12345 <bimalesh54321@gmail.com>
2019-06-24 16:27:13 +0530

    Updates

---
+++ precache-manifest.0e8faf4d9b9ec879ab66ab93174d12fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7244567d7a00283ece092a9551a27738",
+    "url": "/book-list/index.html"
+  },
+  {
+    "revision": "869cb0b273e48941855c",
+    "url": "/book-list/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d53aff71592fccfb556f",
+    "url": "/book-list/static/css/main.d0d2c92f.chunk.css"
+  },
+  {
+    "revision": "869cb0b273e48941855c",
+    "url": "/book-list/static/js/2.700d1e82.chunk.js"
+  },
+  {
+    "revision": "d53aff71592fccfb556f",
+    "url": "/book-list/static/js/main.aa49861e.chunk.js"
+  },
+  {
+    "revision": "370557dc34b895d25c41",
+    "url": "/book-list/static/js/runtime~main.88ddd283.js"
+  }
+]);
