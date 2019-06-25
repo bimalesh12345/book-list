31f4c74bb8cf052e9c5120cd715db91333b5778c
bimalesh12345 <bimalesh54321@gmail.com>
2019-06-25 16:17:27 +0530

    Updates

--- precache-manifest.0e8faf4d9b9ec879ab66ab93174d12fd.js
+++ precache-manifest.0e8faf4d9b9ec879ab66ab93174d12fd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82a8c7fb708a837f6c7167b97e818181",
+    "revision": "7244567d7a00283ece092a9551a27738",
     "url": "/book-list/index.html"
   },
   {
-    "revision": "71acdf882ece06ae3798",
+    "revision": "869cb0b273e48941855c",
     "url": "/book-list/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "ce089a713765b8dce768",
+    "revision": "d53aff71592fccfb556f",
     "url": "/book-list/static/css/main.d0d2c92f.chunk.css"
   },
   {
-    "revision": "71acdf882ece06ae3798",
-    "url": "/book-list/static/js/2.9e24882a.chunk.js"
+    "revision": "869cb0b273e48941855c",
+    "url": "/book-list/static/js/2.700d1e82.chunk.js"
   },
   {
-    "revision": "ce089a713765b8dce768",
-    "url": "/book-list/static/js/main.462c247e.chunk.js"
+    "revision": "d53aff71592fccfb556f",
+    "url": "/book-list/static/js/main.aa49861e.chunk.js"
   },
   {
     "revision": "370557dc34b895d25c41",
